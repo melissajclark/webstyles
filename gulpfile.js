98659d7965407c9134b69cc94bb7dd2d4f1ebcb3
“Melissa” <melissa@melissajclark.ca>
2015-04-03 16:32:28 -0400

    first commit, basic starter files

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    newer = require('gulp-newer');
+
+gulp.task('styles', function(){
+  return gulp.src(['scss/*.scss',
+                   'scss/**/*.scss'], 
+            {base: 'scss/'} )
+      .pipe(plumber())
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('watch', function() {
+      // Watch .scss files
+      gulp.watch('scss/*.scss', ['styles']);
+      gulp.watch('scss/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch']);
