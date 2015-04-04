2a8993b01db8735dec1a84786dc044c99fdb6979
“Melissa” <melissa@melissajclark.ca>
2015-04-03 21:42:18 -0400

    set up to ouput hex via http://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 $(document).ready(function(){
 
 	$(".paletteSwatch").each(function( index ){
-		var color = $(this).css('background-color');
-		$(this).html("<span class='backgrondColour'>" + color);
+		var rgb = $(this).css('backgroundColor').match(/\d+/g);
+		var hex = '#'+ String('0' + Number(rgb[0]).toString(16)).slice(-2) + String('0' + Number(rgb[1]).toString(16)).slice(-2) + String('0' + Number(rgb[2]).toString(16)).slice(-2);
+		$(this).html("<span class='backgrondColour'>" + hex);
 	});
 
 }); // doc ready
