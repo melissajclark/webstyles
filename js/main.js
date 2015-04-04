18731fe6e898b4c6543421fbb45b2adf8b89f9cf
“Melissa” <melissa@melissajclark.ca>
2015-04-03 22:08:10 -0400

    set up function to reveal hex code on click of item

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@ $(document).ready(function(){
 	$(".paletteSwatch").each(function( index ){
 		var rgb = $(this).css('backgroundColor').match(/\d+/g);
 		var hex = '#'+ String('0' + Number(rgb[0]).toString(16)).slice(-2) + String('0' + Number(rgb[1]).toString(16)).slice(-2) + String('0' + Number(rgb[2]).toString(16)).slice(-2);
-		$(this).html("<span class='backgrondColour'>" + hex);
+		$(this).html("<p style='display:none;' class='swatchColour'>" + hex);
+	});
+
+	// reveal the Hex code on click
+	$('.paletteSwatch').each(function( index ){
+		$(this).on("click", function(){
+			$(this).find('p.swatchColour').fadeIn(1000);
+		});
 	});
 
 }); // doc ready
