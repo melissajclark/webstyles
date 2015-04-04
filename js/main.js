bb267d4632191cecae30972db0761d417db074b0
“Melissa” <melissa@melissajclark.ca>
2015-04-03 21:35:46 -0400

    removd index count on appended output of colors

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 
 	$(".paletteSwatch").each(function( index ){
-		var color = (index + ": " + $(this).css('background-color') );
+		var color = $(this).css('background-color');
 		$(this).html("<span class='backgrondColour'>" + color);
 	});
 
