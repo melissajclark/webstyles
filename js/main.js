900a16268e48bb90986791f3f5c8c02f875b70a3
“Melissa” <melissa@melissajclark.ca>
2015-04-04 14:46:45 -0400

    added comments explaining each function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,50 @@
 
 $(document).ready(function(){
 
-	$("a.paletteSwatch").each(function( index ){
-		var rgb = $(this).css('backgroundColor').match(/\d+/g);
-		var hex = '#'+ String('0' + Number(rgb[0]).toString(16)).slice(-2) + String('0' + Number(rgb[1]).toString(16)).slice(-2) + String('0' + Number(rgb[2]).toString(16)).slice(-2);
-		$(this).html("<span style='display:none;' class='swatchColour'>" + hex);
-	});
-
-	// reveal the Hex code on click
-	$('a.paletteSwatch').each(function( index ){
-		$(this).on("click", function(e){
-			e.preventDefault();
-			$(this).find('span.swatchColour').show();
+	/**
+	*
+	* Functions on Swatch Colours:
+	*   - Loop through swatches and get background colour
+	*   - On click of swatch, display the colour as hex code
+	*
+	**/
+	
+		$("a.paletteSwatch").each(function( index ){
+			var rgb = $(this).css('backgroundColor').match(/\d+/g);
+			var hex = '#'+ String('0' + Number(rgb[0]).toString(16)).slice(-2) + String('0' + Number(rgb[1]).toString(16)).slice(-2) + String('0' + Number(rgb[2]).toString(16)).slice(-2);
+			$(this).html("<span style='display:none;' class='swatchColour'>" + hex);
 		});
-	});
 
-	$("button.revealCode").on("click", function(e){
-		e.preventDefault();
+		// reveal the Hex code on click
+		$('a.paletteSwatch').each(function( index ){
+			$(this).on("click", function(e){
+				e.preventDefault();
+				$(this).find('span.swatchColour').show();
+			});
+		});
 
-		var codeExampleHidden = $(".codeExample").hasClass('hidden');
+	/* / end functions on swatch colours */
 
-		if (codeExampleHidden == true) {
-			$('.codeExample').show().removeClass('hidden');
-		} else if (codeExampleHidden == false){
-			$('.codeExample').hide().addClass('hidden');
-		};		
-	}); // end function on click of code button
+	/**
+	*
+	* Function on Click of Code Reveal Button:
+	*   - check if it has the class of hidden
+	*   - if so, reveal it
+	*   - if not, hide it
+	**/
+
+		$("button.revealCode").on("click", function(e){
+			e.preventDefault();
+
+			var codeExampleHidden = $(".codeExample").hasClass('hidden');
+
+			if (codeExampleHidden == true) {
+				$('.codeExample').show().removeClass('hidden');
+			} else if (codeExampleHidden == false){
+				$('.codeExample').hide().addClass('hidden');
+			};		
+		}); // end function on click of code button
+
+	/* / end functions on code reveal button  */
 
 }); // doc ready
