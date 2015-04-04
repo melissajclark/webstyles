a5361da1364e0a52144274dcaf69c6dc2e47c4e4
“Melissa” <melissa@melissajclark.ca>
2015-04-04 00:26:56 -0400

    updated selector to be a.paletteSwatch - means info is available on click / focus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
 
-	$(".paletteSwatch").each(function( index ){
+	$("a.paletteSwatch").each(function( index ){
 		var rgb = $(this).css('backgroundColor').match(/\d+/g);
 		var hex = '#'+ String('0' + Number(rgb[0]).toString(16)).slice(-2) + String('0' + Number(rgb[1]).toString(16)).slice(-2) + String('0' + Number(rgb[2]).toString(16)).slice(-2);
-		$(this).html("<p style='display:none;' class='swatchColour'>" + hex);
+		$(this).html("<span style='display:none;' class='swatchColour'>" + hex);
 	});
 
 	// reveal the Hex code on click
-	$('.paletteSwatch').each(function( index ){
+	$('a.paletteSwatch').each(function( index ){
 		$(this).on("click", function(){
-			$(this).find('p.swatchColour').fadeIn(1000);
+			$(this).find('span.swatchColour').show();
 		});
 	});
 
